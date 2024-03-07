8bda0e20811b68a69af912489e8849a50aba63f6
Hugo DesRosiers <hugo.desrosiers@boardingcities.com>
2024-03-07 21:59:27 +0400

    Improved alignement and coverage of the landing page image and buttons.

--- src/components/anonymousHero.tsx
+++ src/components/anonymousHero.tsx
@@ -40,22 +40,26 @@ export default function AnonymousHero() {
   
   
   return (<>
-    <section className="bg-[url('/img/bugOnAJourney.jpg')] bg-no-repeat bg-cover bg-bottom">
-      <div className="relative py-8 px-4 mx-auto max-w-screen-xl text-white lg:py-16 z-1">
-        <div className="mb-6 max-w-screen-lg lg:mb-0">
-          <div className="flex flex-cols">
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl">FUDD</h1>
-          <p className="rounded-2xl text-center mb-6 ml-6 sm:max-w-64 font-light text-gray-200 bg-gray-900/50 dark:text-gray-100 lg:mb-8 md:text-lg lg:text-xl">
-            Fear, Uncertainty, Doubt: Debunked! An advanced functional programming ecosystem for the web.
-          </p>
+    <section className="h-[50vh] md:h-[70vh] lg:h-[70vh] bg-[url('/img/bugOnAJourney_2.jpg')] bg-no-repeat bg-cover bg-center">
+      <div className="mx-auto max-w-screen-xl text-white z-1">
+        <div className="max-w-screen-lg">
+          <div className="p-4 flex flex-cols">
+            <p className="text-4xl font-extrabold leading-none md:text-5xl lg:text-6xl">
+              <button className="text-center rounded-full p-3 tracking-widest text-rose-600 bg-gray-100/70">FUDD</button>
+            </p>
+            <p className="mb-6 ml-6 sm:max-w-64 rounded-2xl text-center inline-flex items-center font-light text-gray-200 bg-gray-900/50 dark:text-gray-100 lg:mb-8 md:text-lg lg:text-xl">
+              Fear, Uncertainty, Doubt: Debunked! An advanced functional programming ecosystem for the web.
+            </p>
           </div>
-          <a href="/tutorial" className="inline-flex items-center py-3 px-5 font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-900 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-              Tutorial
-              <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-          </a>
-          <a href="/documentation" className="ml-4 inline-flex items-center py-3 px-5 font-medium text-center text-white rounded-lg bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-900 dark:bg-grey-600 dark:hover:bg-grey-700 dark:focus:ring-gray-800">
-              Read the docs
-          </a>
+          <div className="pt-40 md:pt-96 lg:pt-96 pl-4">
+            <a href="/tutorial" className="inline-flex items-center py-3 px-5 font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-900 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                Tutorial
+                <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            </a>
+            <a href="/documentation" className="ml-4 inline-flex items-center py-3 px-5 font-medium text-center text-white rounded-lg bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-900 dark:bg-grey-600 dark:hover:bg-grey-700 dark:focus:ring-gray-800">
+                Read the docs
+            </a>
+          </div>
         </div> 
       </div>
     </section>
