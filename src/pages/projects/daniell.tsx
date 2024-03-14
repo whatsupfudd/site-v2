38453dd6117dc1542437b9d17d459f3d1811662e
Hugo DesRosiers <hugo.desrosiers@boardingcities.com>
2024-03-14 09:36:54 +0400

    One more error on Vercel that doesnt shows up locally: invalid element variable (prop) name.

--- src/pages/projects/daniell.tsx
+++ src/pages/projects/daniell.tsx
@@ -2,7 +2,7 @@ import Navigation from "~/components/navigation"
 
 export default function DaniellPage() {
   return (<>
-    <Navigation current={"DANIELL"} />
+    <Navigation currentItem={"DANIELL"} />
     <section className="text-black dark:text-white">
       <h1 className="text-4xl text-center py-4 text-rose-900 dark:text-rose-400">Daniell</h1>
       <h2 className="text-xl text-center pb-4">A fast and versatile website builder compatible with Hugo, Next.js and Gatsby.</h2>
--- src/pages/projects/fuddle.tsx
+++ src/pages/projects/fuddle.tsx
@@ -2,7 +2,7 @@ import Navigation from "~/components/navigation"
 
 export default function FuddlePage() {
   return (<>
-    <Navigation current={"PROJECTS/FUDDLE"} />
+    <Navigation currentItem={"PROJECTS/FUDDLE"} />
     <section className="px-4 text-black dark:text-white">
       <h1 className="text-4xl text-center py-4 text-rose-900 dark:text-rose-400">Fuddle</h1>
       <h2 className="text-xl text-center pb-4">Your friendly lightweight functional language that loves a reliable web.</h2>
